8c5b11d4967d3096bb6e1867a9216c7f579dca49
Luiz Gemmer <luiz.o.gemmer@gmail.com>
2025-07-08 21:42:59 -0300

    updated docker compose

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python -m gunicorn --bind 0.0.0.0:8000 --workers 3 todo.wsgi:application
+python -m gunicorn --bind 0.0.0.0:8000 --workers 1 todo.wsgi:application
