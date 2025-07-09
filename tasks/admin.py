e9737a0595e1114cb44bdbce7653aae4006937b5
Luiz Gemmer <luiz.o.gemmer@gmail.com>
2025-07-09 20:35:12 -0300

    Testing table creation pipeline

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Tasks # , Category
+from .models import Tasks, Test # , Category
 
 # Register your models here.
 admin.site.register(Tasks)
+admin.site.register(Test)
 #admin.site.register(Category)
 
